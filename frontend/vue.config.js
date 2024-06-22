b06918fa89685fac90a99bd4474ed7b973a30a68
Air-KS <kevinrogeret@gmail.com>
2024-06-22 22:55:45 +0200

    Force redeployement

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,13 +1,9 @@
-const { defineConfig } = require('@vue/cli-service');
 const webpack = require('webpack');
+const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-  publicPath: '/',
-  outputDir: 'dist',
-  assetsDir: 'assets',
-  productionSourceMap: false,
   configureWebpack: {
     output: {
       filename: '[name].[contenthash].js',
@@ -19,14 +15,6 @@ module.exports = defineConfig({
         '__VUE_PROD_DEVTOOLS__': JSON.stringify(false),
         '__VUE_PROD_HYDRATION_MISMATCH_DETAILS__': JSON.stringify(false)
       })
-    ],
-    optimization: {
-      splitChunks: {
-        chunks: 'all'
-      }
-    }
-  },
-  devServer: {
-    proxy: 'http://localhost:8080'
+    ]
   }
 });
